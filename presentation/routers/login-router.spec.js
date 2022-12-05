637fce1de5bbe9c7da99b12dde46cfbe5c9350f1
David Santos <davidprocode@proton.me>
2022-12-05 18:26:15 +0000

    Ensure LoginRouter call EmailValidator with correct email

--- presentation/routers/login-router.spec.js
+++ presentation/routers/login-router.spec.js
@@ -27,6 +27,7 @@ const makeAuthUseCaseWithError = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -230,5 +231,17 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toEqual(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Shoud call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
